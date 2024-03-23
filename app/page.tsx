95fe9cf628559ba1c6f9e50f62f443586eb15c41
devden7 <devdenn7@gmail.com>
2024-03-23 10:36:36 +0700

    create navbar and implement dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-     <div>
-      <h1>test</h1>
+      <div>
+        <h1 className="text-2xl mt-5 font-bold">Latihan NextJs & GraphQL</h1>
       </div>
     </main>
   );
