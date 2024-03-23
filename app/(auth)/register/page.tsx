9028a1841ccfc18794f2ba050882273b948c9acb
devden7 <devdenn7@gmail.com>
2024-03-23 11:49:38 +0700

    slicing register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+import { registerFormSchema } from '@/lib/validations';
+
+const page = () => {
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      username: '',
+      name: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof registerFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Register</h2>
+      <Card className="w-1/3 mt-5">
+        <CardContent className="flex flex-col gap-2 mt-5">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nama Lengkap</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nama Lengkap" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="password"
+                        {...field}
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default page;
--- lib/validations.ts
+++ lib/validations.ts
@@ -4,3 +4,9 @@ export const loginFormSchema = z.object({
   username: z.string().min(1, { message: 'Username tidak boleh kosong' }),
   password: z.string().min(1, { message: 'Password tidak boleh kosong' }),
 });
+
+export const registerFormSchema = z.object({
+  username: z.string().min(1, { message: 'Username tidak boleh kosong' }),
+  name: z.string().min(1, { message: 'Name tidak boleh kosong' }),
+  password: z.string().min(1, { message: 'Password tidak boleh kosong' }),
+});
